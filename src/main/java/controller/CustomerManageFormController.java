ba10a9d048089f7826f3de1b4e5bd188c22bc560
Chathura Madhusanka <madhusanka.slc@gmail.com>
2022-08-05 15:22:35 +0530

    Create Customer GUI

--- src/main/java/controller/CustomerManageFormController.java
+++ src/main/java/controller/CustomerManageFormController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class CustomerManageFormController {
+    public Button btnAdd;
+    public JFXTextField txtCustomerName;
+    public Button btnSave;
+    public JFXTextField txtCustomerAddress;
+    public Button btnDelete;
+    public JFXTextField txtCustomerContact;
+    public TableView tblCustomer;
+    public JFXTextField txtCustomerSearch;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
 }
