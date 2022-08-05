8bbd7419290a10904b7d76245b0ba5e7fcea035c
Chathura Madhusanka <madhusanka.slc@gmail.com>
2022-08-05 14:42:58 +0530

    Create Customer Class

---
+++ src/main/java/util/Customer.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    private String name;
+    private String address;
+    private String contact;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String address, String contact) {
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
